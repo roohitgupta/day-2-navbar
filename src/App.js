03a5479fe56021db9791f4b5fad5ed4f316a3345
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-04-17 12:01:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+ 
+//     <Static/>
+
+//     </div>
+//   );
+// }
+
+// function Static(){
+
+//   return (<div className="App">
+//   <div>
+//   <h3>Mobile Operating System</h3>
+//   <li>Android </li>
+//   <li>Blackberry </li>
+//   <li>iPhone</li>
+//   <li>Windows Phone</li>
+//   </div>
+//   <div>
+//   <h3>Mobile Manufacturers</h3>
+//   <li>Samsung</li>
+//   <li>HTC</li>
+//   <li>Micromax</li>
+//   <li>Apple</li>
+//   </div>
+
+// </div>)
+
+// }
+
+// ======================
+
 function App() {
+  const links = ["Services", "Projects", "About"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     
+          <Logo/>
+
+          <div className="links">
+          {links.map((e)=>{
+                return <Links s={e}/>
+          })}
+          </div>
+         
+          <Button/>
+
+    </div> 
+  )
+};
+
+
+function Logo(){
+  return <h3 className="Logo">Logo Here</h3>
 }
+
+function Links({s}){
+  return <p>{s}</p>
+}
+
+function Button(){
+  return <button className="button">Create</button>
+}
+
+
 
 export default App;
